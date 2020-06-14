b9d4c1bda5bcf8301c8fd0c7fc9f781910e50681
maskai <qorcjftns2@gmail.com>
2020-06-14 14:17:12 -0400

    todo added

--- engine/grammar/object/index.js
+++ engine/grammar/object/index.js
@@ -6,7 +6,7 @@ export class DefaultGrammarRule {
         this.tag = tag;
         this.attr = attr;
     }
-    render(text) {
+    render(text, options = {}) {
         var attr = "";
         for(var i = 0 ; i < this.attr.attribute ; i++) attr += " " + this.attr.attribute[i][0] + "=" + this.attr.attribute[i][1];
         return {
@@ -16,3 +16,32 @@ export class DefaultGrammarRule {
         };
     }
 }
+
+
+
+export class ListGrammarRule {
+    constructor(startTag, endTag, tag, attr = {}) {
+        this.startTag = startTag;
+        this.endTag = endTag;
+        this.tag = tag;
+        this.attr = attr;
+    }
+
+    /**
+     * Render List as ul / li form
+     * @param {String} text String data of current list item (li) 
+     * @param {Object} options 
+     *      - options.type: determine if this item is first or last: 1 if first, -1 if last, 0 otherwise.
+     */
+    render(text, options = {}) {
+        // TODO - render
+
+    }
+}
+
+var grammar = new ListGrammarRule("\n * ", "\n", "ul", {attribute: ['class', 'wiki-list']});
+grammar.render("test 1", {order: 1}); // -> <ul class="wiki-list"><li>test 1</li>
+grammar.render("test 2", {order: 0}); // -> <li>test 2</li>
+grammar.render("test 3", {order: 0}); // -> <li>test 3</li>
+grammar.render("test 4", {order: 0}); // -> <li>test 4</li>
+grammar.render("test 5", {order: -1}); // -> <li>test 5</li></ul>
